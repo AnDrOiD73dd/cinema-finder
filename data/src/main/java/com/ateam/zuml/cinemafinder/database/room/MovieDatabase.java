83d95c3dc23e8cabd0b719afcc4268755c1c5e53
Viktor <vm.orion@gmail.com>
2018-11-04 19:53:05 +0300

    Reduce MovieDatabase class.

--- data/src/main/java/com/ateam/zuml/cinemafinder/database/room/MovieDatabase.java
+++ data/src/main/java/com/ateam/zuml/cinemafinder/database/room/MovieDatabase.java
@@ -2,9 +2,7 @@ package com.ateam.zuml.cinemafinder.database.room;
 
 
 import android.arch.persistence.room.Database;
-import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
-import android.content.Context;
 
 import com.ateam.zuml.cinemafinder.database.room.daos.MovieDao;
 import com.ateam.zuml.cinemafinder.database.room.model.environmet.CountryEntity;
@@ -24,21 +22,5 @@ import com.ateam.zuml.cinemafinder.database.room.model.movie.ProductionCountries
 public abstract class MovieDatabase extends RoomDatabase {
 
     public abstract MovieDao movieDao();
-
-    private static MovieDatabase DB_INSTANCE;
-
-    public static MovieDatabase getDatabase(final Context context) {
-        if (DB_INSTANCE == null) {
-            synchronized (MovieDatabase.class) {
-                if (DB_INSTANCE == null) {
-                    // Create database here
-                    DB_INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                            MovieDatabase.class, "zuml_database")
-                            .build();
-                }
-            }
-        }
-        return DB_INSTANCE;
-    }
 
 }
