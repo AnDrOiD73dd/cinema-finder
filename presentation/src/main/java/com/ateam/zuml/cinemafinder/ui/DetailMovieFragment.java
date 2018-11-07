a57081dd60e2eff635932d453c3efc2d4ae08a90
Evgeny Uleykin <euleykin@getmobit.ru>
2018-11-07 11:18:58 +0300

    Add show Snackbar when showError called

--- presentation/src/main/java/com/ateam/zuml/cinemafinder/ui/DetailMovieFragment.java
+++ presentation/src/main/java/com/ateam/zuml/cinemafinder/ui/DetailMovieFragment.java
@@ -3,6 +3,8 @@ package com.ateam.zuml.cinemafinder.ui;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.widget.AppCompatImageView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -30,6 +32,8 @@ public class DetailMovieFragment extends MvpAppCompatFragment implements DetailM
     @InjectPresenter
     DetailMoviePresenter presenter;
 
+    @BindView(R.id.movie_detail_root)
+    ConstraintLayout rootView;
     @BindView(R.id.tv_movie_title)
     TextView titleView;
     @BindView(R.id.tv_movie_original_title)
@@ -111,7 +115,7 @@ public class DetailMovieFragment extends MvpAppCompatFragment implements DetailM
 
     @Override
     public void showError() {
-
+        Snackbar.make(rootView, R.string.movie_details_error_message, Snackbar.LENGTH_LONG).show();
     }
 
     @Override
