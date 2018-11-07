b0b6b60ae7435c2cf121d8cf1ff85209c9d531b1
Evgeny Uleykin <euleykin@getmobit.ru>
2018-11-07 10:49:00 +0300

    Inject StringUtil to SearchResponsePresenter

--- presentation/src/main/java/com/ateam/zuml/cinemafinder/presentation/presenter/SearchResponsePresenter.java
+++ presentation/src/main/java/com/ateam/zuml/cinemafinder/presentation/presenter/SearchResponsePresenter.java
@@ -8,6 +8,7 @@ import com.ateam.zuml.cinemafinder.presentation.view.SearchResponseView;
 import com.ateam.zuml.cinemafinder.presentation.view.SearchRowView;
 import com.ateam.zuml.cinemafinder.ui.Screens;
 import com.ateam.zuml.cinemafinder.util.Constants;
+import com.ateam.zuml.cinemafinder.util.StringUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,53 +18,13 @@ import javax.inject.Named;
 
 import ru.terrakok.cicerone.Router;
 
+
 @InjectViewState
 public class SearchResponsePresenter extends MvpPresenter<SearchResponseView> {
 
-    public final class SearchListPresenter {
-
-        private List<MovieListModel> searchList;
-
-        SearchListPresenter() {
-            this.searchList = new ArrayList<>();
-        }
-
-        public void bindViewAt(SearchRowView view, int position) {
-            //FIXME 06.11.2018 add Picasso
-            view.setPoster(R.drawable.ic_broken_image);
-            view.setTitle(searchList.get(position).getTitle());
-            view.setOriginalTitle(searchList.get(position).getOriginalTitle());
-            view.setReleaseDate(searchList.get(position).getReleaseYear());
-            view.setGenres(getStringFromArrayGenres(searchList.get(position).getGenres()));
-            view.setVoteAverage(searchList.get(position).getVoteAverage());
-        }
-
-        public int getSearchCount() {
-            return searchList.size();
-        }
-
-        public void setSearchList(List<MovieListModel> searchList) {
-            this.searchList = searchList;
-        }
-
-        private String getStringFromArrayGenres(String[] arr) {
-            StringBuilder sb = new StringBuilder();
-
-            for (int i = 0; i < arr.length; i++) {
-                sb.append(arr[i]);
-                if (arr.length == 1 || i == (arr.length - 1)) {
-                    break;
-                }
-                sb.append(", ");
-            }
-
-            return sb.toString();
-        }
-    }
-
     @Named(Constants.MAIN_CONTAINER)
-    @Inject
-    Router router;
+    @Inject Router router;
+    @Inject StringUtil stringUtil;
 
     private SearchListPresenter listPresenter;
 
@@ -81,4 +42,32 @@ public class SearchResponsePresenter extends MvpPresenter<SearchResponseView> {
         getViewState().closeSearch();
         router.navigateTo(new Screens.DetailMovieScreen(movieId));
     }
+
+    public final class SearchListPresenter {
+        private List<MovieListModel> searchList;
+
+        SearchListPresenter() {
+            this.searchList = new ArrayList<>();
+        }
+
+        public void bindViewAt(SearchRowView view, int position) {
+            //FIXME 06.11.2018 add Picasso
+            view.setPoster(R.drawable.ic_broken_image);
+            view.setTitle(searchList.get(position).getTitle());
+            view.setOriginalTitle(searchList.get(position).getOriginalTitle());
+            view.setReleaseDate(searchList.get(position).getReleaseYear());
+            view.setGenres(stringUtil.getStringFromArrayGenres(searchList.get(position).getGenres()));
+            view.setVoteAverage(searchList.get(position).getVoteAverage());
+        }
+
+        public int getSearchCount() {
+            return searchList.size();
+        }
+
+        public void setSearchList(List<MovieListModel> searchList) {
+            this.searchList = searchList;
+        }
+    }
+
 }
+
