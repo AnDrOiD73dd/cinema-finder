4c21a57dea78ebcf65938d2c13b33647166ac567
Vitaly <al-not@ya.ru>
2018-11-12 00:17:37 +0500

    fix enum collections

--- presentation/src/main/java/com/ateam/zuml/cinemafinder/util/CollectionsRow.java
+++ presentation/src/main/java/com/ateam/zuml/cinemafinder/util/CollectionsRow.java
@@ -3,10 +3,8 @@ package com.ateam.zuml.cinemafinder.util;
 import com.ateam.zuml.cinemafinder.R;
 
 public enum CollectionsRow {
-    POPULAR(R.string.category_title_popular),
     UPCOMING(R.string.category_title_upcoming),
     NOW_PLAYING(R.string.category_title_now_playing),
-    LATEST(R.string.category_title_latest),
     NONE(R.string.category_title_none);
 
     private int title;
