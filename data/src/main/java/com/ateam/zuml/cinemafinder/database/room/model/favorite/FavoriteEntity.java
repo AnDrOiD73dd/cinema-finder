ae172653d6dbae425e1b8a48f73aa7bfe0108319
dimasan92 <dimasan92@gmail.com>
2018-11-12 22:45:31 +0300

    updating entities for movies and them favorites

--- data/src/main/java/com/ateam/zuml/cinemafinder/database/room/model/favorite/FavoriteEntity.java
+++ data/src/main/java/com/ateam/zuml/cinemafinder/database/room/model/favorite/FavoriteEntity.java
@@ -1,20 +1,24 @@
 package com.ateam.zuml.cinemafinder.database.room.model.favorite;
 
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
 import android.arch.persistence.room.PrimaryKey;
 
+import com.ateam.zuml.cinemafinder.database.room.model.movie.MovieEntity;
 import com.google.gson.annotations.SerializedName;
 
-@Entity(tableName = "favorites")
+@Entity(tableName = "favorites",
+        foreignKeys = @ForeignKey(entity = MovieEntity.class, parentColumns = "id",
+                childColumns = "movie_id", onDelete = ForeignKey.CASCADE))
 public final class FavoriteEntity {
 
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
     private final int id;
 
     @SerializedName("movie_id")
     private final int movieId;
 
-    public FavoriteEntity(int id, int movieId) {
+    public FavoriteEntity(final int id, final int movieId) {
         this.id = id;
         this.movieId = movieId;
     }
--- data/src/main/java/com/ateam/zuml/cinemafinder/database/room/model/movie/MovieEntity.java
+++ data/src/main/java/com/ateam/zuml/cinemafinder/database/room/model/movie/MovieEntity.java
@@ -2,141 +2,60 @@ package com.ateam.zuml.cinemafinder.database.room.model.movie;
 
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.Ignore;
-import android.arch.persistence.room.Relation;
-import android.arch.persistence.room.TypeConverters;
-import android.support.annotation.NonNull;
+import android.arch.persistence.room.PrimaryKey;
 
-import com.ateam.zuml.cinemafinder.database.room.DateConverters;
-import com.ateam.zuml.cinemafinder.database.room.MovieStatusConverter;
-import com.ateam.zuml.cinemafinder.database.room.model.enums.MovieStatus;
-
-import java.util.Date;
-import java.util.List;
-
-@Entity(tableName = "movies", primaryKeys = "id")
+@Entity(tableName = "movies")
 public final class MovieEntity {
 
+    @PrimaryKey
     private int id;
-
-    @NonNull private final String title;
-
-    private final boolean adult;
-
-    @NonNull private final String homepage;
-
-    @NonNull private final String overview;
-
+    private final String title;
+    private final String originalTitle;
     @ColumnInfo(name = "poster_path")
-    @NonNull
     private final String posterPath;
-
     @ColumnInfo(name = "release_data")
-    @TypeConverters(DateConverters.class)
-    @NonNull
-    private final Date releaseDate;
-
-    @TypeConverters(MovieStatusConverter.class)
-    @NonNull
-    private final MovieStatus status;  //Allowed Values: Rumored, Planned, In Production, Post Production, Released, Canceled
-
-    @NonNull private final String tagline;
-
-    private final int runtime;
-
+    private final String releaseDate;
     @ColumnInfo(name = "vote_average")
-    private final long voteAverage;
+    private final int voteAverage;
+    private String[] genres;
 
-    @ColumnInfo(name = "vote_count")
-    private final int voteCount;
-
-    @Ignore
-    @Relation(entity = MovieGenresEntity.class, parentColumn = "id", entityColumn = "movie_id")
-    private List<MovieGenresEntity> genres;
-
-    @Ignore
-    @Relation(entity = ProductionCountriesEntity.class, parentColumn = "id", entityColumn = "movie_id")
-    private List<ProductionCountriesEntity> productionCountries;
-
-
-    public MovieEntity(int id, @NonNull String title, boolean adult, @NonNull String homepage,
-                       @NonNull String overview, @NonNull String posterPath, @NonNull Date releaseDate,
-                       @NonNull MovieStatus status, @NonNull String tagline, int runtime,
-                       long voteAverage, int voteCount) {
+    public MovieEntity(final int id, final String title, final String originalTitle,
+                       final String posterPath, final String releaseDate, final int voteAverage,
+                       final String[] genres) {
         this.id = id;
         this.title = title;
-        this.adult = adult;
-        this.homepage = homepage;
-        this.overview = overview;
+        this.originalTitle = originalTitle;
         this.posterPath = posterPath;
         this.releaseDate = releaseDate;
-        this.status = status;
-        this.tagline = tagline;
-        this.runtime = runtime;
         this.voteAverage = voteAverage;
-        this.voteCount = voteCount;
+        this.genres = genres;
     }
 
     public int getId() {
         return id;
     }
 
-    @NonNull
     public String getTitle() {
         return title;
     }
 
-    public boolean isAdult() {
-        return adult;
+    public String getOriginalTitle() {
+        return originalTitle;
     }
 
-    @NonNull
-    public String getHomepage() {
-        return homepage;
-    }
-
-    @NonNull
-    public String getOverview() {
-        return overview;
-    }
-
-    @NonNull
     public String getPosterPath() {
         return posterPath;
     }
 
-    @NonNull
-    public Date getReleaseDate() {
+    public String getReleaseDate() {
         return releaseDate;
     }
 
-    @NonNull
-    public MovieStatus getStatus() {
-        return status;
-    }
-
-    @NonNull
-    public String getTagline() {
-        return tagline;
-    }
-
-    public Integer getRuntime() {
-        return runtime;
-    }
-
-    public long getVoteAverage() {
+    public int getVoteAverage() {
         return voteAverage;
     }
 
-    public int getVoteCount() {
-        return voteCount;
-    }
-
-    public List<MovieGenresEntity> getGenres() {
+    public String[] getGenres() {
         return genres;
-    }
-
-    public List<ProductionCountriesEntity> getProductionCountries() {
-        return productionCountries;
     }
 }
