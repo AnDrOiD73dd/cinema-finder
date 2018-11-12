974f63106ab0d9788b134eba3c28b5d90646e427
dimasan92 <dimasan92@gmail.com>
2018-11-12 22:20:27 +0300

    stub for favorites repository

--- data/src/main/java/com/ateam/zuml/cinemafinder/favorites/FavoritesRepositoryImpl.java
+++ data/src/main/java/com/ateam/zuml/cinemafinder/favorites/FavoritesRepositoryImpl.java
@@ -3,8 +3,8 @@ package com.ateam.zuml.cinemafinder.favorites;
 import com.ateam.zuml.cinemafinder.database.room.MovieDatabase;
 import com.ateam.zuml.cinemafinder.database.room.daos.FavoritesDao;
 import com.ateam.zuml.cinemafinder.mapper.MovieMapper;
-import com.ateam.zuml.cinemafinder.model.characteristic.LogoSize;
 import com.ateam.zuml.cinemafinder.model.movie.MovieDetailsModel;
+import com.ateam.zuml.cinemafinder.model.movie.MovieListModel;
 import com.ateam.zuml.cinemafinder.repository.FavoritesRepository;
 
 import java.util.List;
@@ -12,8 +12,8 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import io.reactivex.Completable;
 import io.reactivex.Single;
-import io.reactivex.schedulers.Schedulers;
 
 @Singleton
 public class FavoritesRepositoryImpl implements FavoritesRepository {
@@ -22,35 +22,28 @@ public class FavoritesRepositoryImpl implements FavoritesRepository {
     private final MovieMapper movieMapper;
 
     @Inject
-    public FavoritesRepositoryImpl(MovieDatabase movieDatabase, final MovieMapper movieMapper) {
+    public FavoritesRepositoryImpl(final MovieDatabase movieDatabase, final MovieMapper movieMapper) {
         this.db = movieDatabase.favoritesDao();
         this.movieMapper = movieMapper;
     }
 
     @Override
-    public Single<List<MovieDetailsModel>> getAllMovies(final LogoSize logoSize) {
-        return db.getAllMoviesByFavoriteId()
-                .subscribeOn(Schedulers.io())
-                .map(movieEntities -> movieMapper.mapMovieDetailsFromMovieEntity(movieEntities, logoSize));
+    public Single<List<MovieListModel>> getAllMovies() {
+        return null;
     }
 
     @Override
-    public void addMovie(MovieDetailsModel movieDetailsModel) {
-        db.insert(movieMapper.mapFavoriteEntityFromMovieDetailsModel(movieDetailsModel));
+    public Completable addMovie(MovieDetailsModel movieDetailsModel) {
+        return Completable.complete();
     }
 
     @Override
-    public void addAllMovies(List<MovieDetailsModel> movieDetailsModels) {
-
+    public Completable addMovie(MovieListModel movieDetailsModel) {
+        return Completable.complete();
     }
 
     @Override
-    public void removeMovie(MovieDetailsModel movieDetailsModel) {
-
-    }
-
-    @Override
-    public void removeAllMovies(List<MovieDetailsModel> movieDetailsModels) {
-
+    public Completable removeMovie(int id) {
+        return null;
     }
 }
