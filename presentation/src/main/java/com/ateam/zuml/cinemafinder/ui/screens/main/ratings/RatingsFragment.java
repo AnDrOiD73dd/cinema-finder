000b991df045f7d0c99f6f96e3b33592f7843b86
Vitaly <al-not@ya.ru>
2018-11-13 16:31:16 +0500

    add methods in interface

--- presentation/src/main/java/com/ateam/zuml/cinemafinder/ui/screens/main/ratings/RatingsFragment.java
+++ presentation/src/main/java/com/ateam/zuml/cinemafinder/ui/screens/main/ratings/RatingsFragment.java
@@ -45,6 +45,93 @@ public class RatingsFragment extends BaseFragment implements RatingsView, BackBu
         inflater.inflate(R.menu.menu_main, menu);
     }
 
+    // ###################################### RatingsView ########################################
+
+    @Override
+    public void updateFirstRow() {
+
+    }
+
+    @Override
+    public void updateSecondRow() {
+
+    }
+
+    @Override
+    public void updateThirdRow() {
+
+    }
+
+    @Override
+    public void updateFourthRow() {
+
+    }
+
+    @Override
+    public void showFirstLoading() {
+
+    }
+
+    @Override
+    public void showSecondLoading() {
+
+    }
+
+    @Override
+    public void showThirdLoading() {
+
+    }
+
+    @Override
+    public void showFourthLoading() {
+
+    }
+
+    @Override
+    public void hideFirstLoading() {
+
+    }
+
+    @Override
+    public void hideSecondLoading() {
+
+    }
+
+    @Override
+    public void hideThirdLoading() {
+
+    }
+
+    @Override
+    public void hideFourthLoading() {
+
+    }
+
+    @Override
+    public void showNoInFirst() {
+
+    }
+
+    @Override
+    public void showNoInSecond() {
+
+    }
+
+    @Override
+    public void showNoInThird() {
+
+    }
+
+    @Override
+    public void showNoInFourth() {
+
+    }
+
+    @Override
+    public void showError() {
+
+    }
+
     // #################################### BackButtonListener ###################################
 
     @Override
--- presentation/src/main/java/com/ateam/zuml/cinemafinder/ui/screens/main/ratings/RatingsView.java
+++ presentation/src/main/java/com/ateam/zuml/cinemafinder/ui/screens/main/ratings/RatingsView.java
@@ -4,4 +4,37 @@ import com.arellomobile.mvp.MvpView;
 
 public interface RatingsView extends MvpView {
 
+    void updateFirstRow();
+
+    void updateSecondRow();
+
+    void updateThirdRow();
+
+    void updateFourthRow();
+
+    void showFirstLoading();
+
+    void showSecondLoading();
+
+    void showThirdLoading();
+
+    void showFourthLoading();
+
+    void hideFirstLoading();
+
+    void hideSecondLoading();
+
+    void hideThirdLoading();
+
+    void hideFourthLoading();
+
+    void showNoInFirst();
+
+    void showNoInSecond();
+
+    void showNoInThird();
+
+    void showNoInFourth();
+
+    void showError();
 }
