6b318f48c17b0a53a4226957e58096927cafe50a
Viktor <vm.orion@gmail.com>
2019-01-15 16:37:28 +0300

    Constants class in data layer is renamed to exclude possible intersections with constants in presentation layer.

--- data/src/main/java/com/ateam/zuml/cinemafinder/util/DataConstants.java
+++ data/src/main/java/com/ateam/zuml/cinemafinder/util/DataConstants.java
@@ -1,6 +1,6 @@
 package com.ateam.zuml.cinemafinder.util;
 
-public final class Constants {
+public final class DataConstants {
 
     public static final String ACTION_GENRE_ID = "28";
     public static final String ANIMATION_GENRE_ID = "16";
