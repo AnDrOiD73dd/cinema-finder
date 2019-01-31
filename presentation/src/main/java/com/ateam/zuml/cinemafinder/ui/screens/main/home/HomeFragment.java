10c675cb2c0139d5ffeaba5056d655110bc3f195
Viktor <vm.orion@gmail.com>
2019-02-01 00:20:21 +0300

    Implementation SwipeRefreshLayout for Home and Rating screens. New item added in toolbar for duplicate this gesture.

--- presentation/src/main/java/com/ateam/zuml/cinemafinder/ui/screens/main/home/HomeFragment.java
+++ presentation/src/main/java/com/ateam/zuml/cinemafinder/ui/screens/main/home/HomeFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -42,6 +44,7 @@ public class HomeFragment extends BaseFragment implements HomeView, BackButtonLi
     @BindView(R.id.rv_upcoming_row) RecyclerView upcomingRecyclerView;
     @BindView(R.id.pb_upcoming_row) ProgressBar upcomingProgressBar;
     @BindView(R.id.tv_upcoming_row_no_results) TextView upcomingNoResultsView;
+    @BindView(R.id.swipe_refresh) SwipeRefreshLayout swipeRefreshLayout;
 
     @Inject ImageLoader imageLoader;
 
@@ -75,6 +78,7 @@ public class HomeFragment extends BaseFragment implements HomeView, BackButtonLi
         ButterKnife.bind(this, v);
         nowPlayingAdapter = new NowPlayingRowAdapter(presenter.getNowPlayingPresenter(), imageLoader);
         upcomingAdapter = new UpcomingRowAdapter(presenter.getUpcomingPresenter(), imageLoader);
+        swipeRefreshLayout.setOnRefreshListener(() -> presenter.onSwipeForRefreshScreen());
     }
 
     private void setupRecyclerView(RecyclerView recyclerView,
@@ -88,6 +92,19 @@ public class HomeFragment extends BaseFragment implements HomeView, BackButtonLi
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.menu_main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh_data:
+                swipeRefreshLayout.setRefreshing(true);
+                presenter.onSwipeForRefreshScreen();
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     //region ### HomeView ###
@@ -133,6 +150,11 @@ public class HomeFragment extends BaseFragment implements HomeView, BackButtonLi
     @Override
     public void showNoInUpcoming() {
         upcomingNoResultsView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void finishSwipeGestureAction() {
+        swipeRefreshLayout.setRefreshing(false);
     }
 
     @Override
--- presentation/src/main/java/com/ateam/zuml/cinemafinder/ui/screens/main/ratings/RatingsFragment.java
+++ presentation/src/main/java/com/ateam/zuml/cinemafinder/ui/screens/main/ratings/RatingsFragment.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -35,6 +37,7 @@ public class RatingsFragment extends BaseFragment implements RatingsView, BackBu
     private RatingsCollectionRowAdapter topComediesAdapter;
     private RatingsCollectionRowAdapter topAnimationsAdapter;
 
+    @BindView(R.id.swipe_refresh) SwipeRefreshLayout swipeRefreshLayout;
     @BindView(R.id.ratings_root) LinearLayout rootView;
 
     @BindView(R.id.tv_top_movies_row_name) TextView topMoviesTitleView;
@@ -92,6 +95,7 @@ public class RatingsFragment extends BaseFragment implements RatingsView, BackBu
         topActionsAdapter = new RatingsCollectionRowAdapter(presenter.getTopActionsPresenter(), imageLoader);
         topComediesAdapter = new RatingsCollectionRowAdapter(presenter.getTopComediesPresenter(), imageLoader);
         topAnimationsAdapter = new RatingsCollectionRowAdapter(presenter.getTopAnimationsPresenter(), imageLoader);
+        swipeRefreshLayout.setOnRefreshListener(() -> presenter.onSwipeForRefreshScreen());
     }
 
     private void setupRecyclerView(RecyclerView recyclerView, RatingsCollectionRowAdapter adapter) {
@@ -104,6 +108,19 @@ public class RatingsFragment extends BaseFragment implements RatingsView, BackBu
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.menu_main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh_data:
+                swipeRefreshLayout.setRefreshing(true);
+                presenter.onSwipeForRefreshScreen();
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     //region ### RatingsView ###
@@ -194,6 +211,11 @@ public class RatingsFragment extends BaseFragment implements RatingsView, BackBu
     @Override
     public void showNoInTopAnimations() {
         topAnimationsNoResultsView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void finishSwipeGestureAction() {
+        swipeRefreshLayout.setRefreshing(false);
     }
 
     @Override
